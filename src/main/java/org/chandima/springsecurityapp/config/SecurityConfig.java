8ff85bfab14b464d752b4f77c73fb04b36ed50b2
chandima2000 <hychandima2000@gmail.com>
2024-10-26 09:02:58 +0530

    Generate JWT token

--- src/main/java/org/chandima/springsecurityapp/config/SecurityConfig.java
+++ src/main/java/org/chandima/springsecurityapp/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package org.chandima.springsecurityapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,7 +34,7 @@ public class SecurityConfig {
 
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("register")
+                        .requestMatchers("register","login")
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
@@ -55,7 +57,10 @@ public class SecurityConfig {
 
     }
 
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
 
 }
--- src/main/java/org/chandima/springsecurityapp/model/User.java
+++ src/main/java/org/chandima/springsecurityapp/model/User.java
@@ -13,7 +13,7 @@ import lombok.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String username;
     private String password;
