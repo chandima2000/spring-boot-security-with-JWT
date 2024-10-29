6ba6cf67070bcd0b9c7cf5d9ceebb156c32fa07d
chandima2000 <hychandima2000@gmail.com>
2024-10-29 18:30:47 +0530

    validating JWT Token and Done the JWT Token part

--- src/main/java/org/chandima/springsecurityapp/service/JwtService.java
+++ src/main/java/org/chandima/springsecurityapp/service/JwtService.java
@@ -1,7 +1,7 @@
 package org.chandima.springsecurityapp.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,13 +9,12 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.function.Function;
 
 
 @Service
@@ -46,12 +45,12 @@ public class JwtService {
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 1000*60))
-                .signWith(getKey(), SignatureAlgorithm.HS256).compact();
+                .signWith(getKey(), Jwts.SIG.HS256).compact();
 
 
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
 
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
 
@@ -59,10 +58,32 @@ public class JwtService {
     }
 
     public String extractUserName(String token) {
-        return " ";
+        // extract the username from jwt token
+        return extractClaim(token, Claims::getSubject);
     }
 
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build().parseSignedClaims(token).getPayload();
+    }
+
+
     public boolean validateToken(String token, UserDetails userDetails) {
-        return true;
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
